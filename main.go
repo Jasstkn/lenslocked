54ce391956b2b141bf2ef552d172199e863a9c0e
Maria Kotlyarevskaya <mariia.kotliarevskaia@gmail.com>
2022-11-26 19:39:10 +0100

    Add routing
    Signed-off-by: Maria Kotlyarevskaya <mariia.kotliarevskaia@gmail.com>

--- main.go
+++ main.go
@@ -10,14 +10,24 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
 }
 
+func pathHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	default:
+		w.WriteHeader(http.StatusNotFound)
+	}
+}
+
 func contactHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, "<h1>Contact page</h1><p>To get in touch, email me at <a href=\"mailto:jasstkn.051@gmail.com\">jasstkn.051@gmail.com</a>.</p>")
 }
 
 func main() {
-	http.HandleFunc("/", homeHandler)
-	http.HandleFunc("/contact", contactHandler)
+	http.HandleFunc("/", pathHandler)
 	fmt.Println("Starting the server on http://localhost:3000")
 	http.ListenAndServe(":3000", nil)
 }
