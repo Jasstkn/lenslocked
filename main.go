124b93ee4b31669733d2f622fe137f0f286e6b3b
Maria Kotlyarevskaya <mariia.kotliarevskaia@gmail.com>
2022-11-27 13:52:14 +0100

    move all pages to templates, create executeTemplate function
    Signed-off-by: Maria Kotlyarevskaya <mariia.kotliarevskaia@gmail.com>

--- main.go
+++ main.go
@@ -12,47 +12,41 @@ import (
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	// build path for any OS
-	tplPath := filepath.Join("templates", "home.gohtml")
-	tpl, err := template.ParseFiles(tplPath)
+	err := executeTemplate(w, "home")
 	if err != nil {
-		log.Printf("parsing template: %v", err)
-		http.Error(w, "There was an error while parsing the template", http.StatusInternalServerError)
-		return
-	}
-	err = tpl.Execute(w, nil)
-	if err != nil {
-		log.Printf("execute teamplate: %v", err)
+		log.Printf("executing template: %v", err)
 		http.Error(w, "There was an error while executing the template", http.StatusInternalServerError)
-		return
 	}
 }
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Contact page</h1><p>To get in touch, email me at <a href=\"mailto:jasstkn.051@gmail.com\">jasstkn.051@gmail.com</a>.</p>")
+	err := executeTemplate(w, "contact")
+	if err != nil {
+		log.Printf("executing template: %v", err)
+		http.Error(w, "There was an error while executing the template", http.StatusInternalServerError)
+	}
 }
 
 func faqHandler(w http.ResponseWriter, r *http.Request) {
-	id := chi.URLParam(r, "id")
-	fmt.Fprint(w, `<h1> This is article #`+id+`</h1>`)
-	fmt.Fprint(w, `
-	<h1>FAQ Page</h1>
-	<ul>
-	<li>
-		<b>Is there a free version?</b><br>
-		Yes! We offer a free trial for 30 days on any paid plans.
-	</li>
-	<li>
-		<b>What are your support hours?</b><br>
-		We have support staff answering emails 24/7, though response
-		times may be a bit slower on weekends.
-	</li>
-	<li>
-		<b>How do I contact support?</b><br>
-		Email us - <a href="mailto:support@lenslocked.com">support@lenslocked.com</a>
-	</li>
-	</ul>
-	`)
+	err := executeTemplate(w, "faq")
+	if err != nil {
+		log.Printf("executing template: %v", err)
+		http.Error(w, "There was an error while executing the template", http.StatusInternalServerError)
+	}
+}
+
+func executeTemplate(w http.ResponseWriter, filename string) error {
+	// build path for any OS
+	tplPath := filepath.Join("templates", filename+".gohtml")
+	tpl, err := template.ParseFiles(tplPath)
+	if err != nil {
+		return err
+	}
+	err = tpl.Execute(w, nil)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func main() {
@@ -60,7 +54,7 @@ func main() {
 	r.Use(middleware.Logger)
 	r.Get("/", homeHandler)
 	r.Get("/contact", contactHandler)
-	r.Get("/faq/{id}", faqHandler)
+	r.Get("/faq", faqHandler)
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "page not found", http.StatusNotFound)
 	})
