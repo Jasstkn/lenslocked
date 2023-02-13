42231711e33ae1544203770e42562660035f791c
Maria Kotlyarevskaya <mariia.kotliarevskaia@gmail.com>
2023-02-13 21:43:23 +0100

    create sample orders
    Signed-off-by: Maria Kotlyarevskaya <mariia.kotliarevskaia@gmail.com>

--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -79,7 +79,7 @@ func main() {
 
 	// fmt.Println("User created. id = ", id)
 
-	id := 6
+	id := 1
 
 	row := db.QueryRow(`
 		SELECT name, email
@@ -92,4 +92,17 @@ func main() {
 		panic(err)
 	}
 	fmt.Printf("User information: name=%s, email=%s\n", name, email)
+
+	userID := 1
+	for i := 1; i < 5; i++ {
+		amount := i * 100
+		desc := fmt.Sprintf("Fake order #%d", i)
+		_, err := db.Exec(`
+			INSERT INTO orders(user_id, amount, description)
+			VALUES($1, $2, $3)`, userID, amount, desc)
+		if err != nil {
+			panic(err)
+		}
+	}
+	fmt.Println("Created fake orders.")
 }
