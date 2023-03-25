21f60dabd6b25b6e0405bc2627d937a913763789
Maria Kotlyarevskaya <mariia.kotliarevskaia@gmail.com>
2023-03-25 15:49:38 +0100

    setup db connection
    Signed-off-by: Maria Kotlyarevskaya <mariia.kotliarevskaia@gmail.com>

--- cmd/exp/exp.go
+++ cmd/exp/exp.go
@@ -9,29 +9,8 @@ import (
 	_ "github.com/jackc/pgx/v4/stdlib"
 )
 
-type PostgresConfig struct {
-	Host     string
-	Port     string
-	User     string
-	Password string
-	Database string
-	SSLMode  string
-}
-
-func (cfg PostgresConfig) String() string {
-	return fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
-		cfg.Host, cfg.Port, cfg.User, cfg.Password, cfg.Database, cfg.SSLMode)
-}
-
 func main() {
-	cfg := PostgresConfig{
-		Host:     "localhost",
-		Port:     "5432",
-		User:     "baloo",
-		Password: "junglebook",
-		Database: "lenslocked",
-		SSLMode:  "disable",
-	}
+	cfg := models.DefaultPostgresConfig()
 	db, err := sql.Open("pgx", cfg.String())
 	defer db.Close()
 
@@ -46,7 +25,7 @@ func main() {
 	fmt.Println("Connection to DB is established!")
 
 	us := models.UserService{DB: db}
-	user, err := us.Create("bob@bob.com", "bob123")
+	user, err := us.Create("bob1@bob.com", "bob123")
 	if err != nil {
 		panic(err)
 	}
