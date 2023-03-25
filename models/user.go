9fcc2e9955287e6d5abd727ffccd6d05ef0524d9
Maria Kotlyarevskaya <mariia.kotliarevskaia@gmail.com>
2023-03-25 13:47:30 +0100

    add UserService type
    Signed-off-by: Maria Kotlyarevskaya <mariia.kotliarevskaia@gmail.com>

--- models/user.go
+++ models/user.go
@@ -1,7 +1,13 @@
 package models
 
+import "database/sql"
+
 type User struct {
 	ID           int
 	Email        string // TODO: validate email
 	PasswordHash string
+}
+
+type UserService struct {
+	DB *sql.DB
 }
