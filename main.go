1386d0242500cff91974e35db551e2547145cbe3
Maria Kotlyarevskaya <mariia.kotliarevskaia@gmail.com>
2023-03-25 16:25:09 +0100

    add signin view
    Signed-off-by: Maria Kotlyarevskaya <mariia.kotliarevskaia@gmail.com>

--- main.go
+++ main.go
@@ -44,7 +44,14 @@ func main() {
 		templates.FS,
 		"signup.gohtml", layoutTpl,
 	))
+	usersC.Templates.SignIn = views.Must(views.ParseFS(
+		templates.FS,
+		"signin.gohtml", layoutTpl,
+	))
+
 	r.Get("/signup", usersC.New)
+	r.Get("/signin", usersC.SignIn)
+	
 	r.Post("/users", usersC.Create)
 
 	r.Get("/faq", controllers.FAQ(
