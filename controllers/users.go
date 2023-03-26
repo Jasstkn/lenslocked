229741566649999bd5145d594e847fe3be5ac53d
Mariia Kotliarevskaia <mariia.kotliarevskaia@gmail.com>
2023-03-26 23:15:28 +0200

    add custom func to tpl
    Signed-off-by: Mariia Kotliarevskaia <mariia.kotliarevskaia@gmail.com>

--- controllers/users.go
+++ controllers/users.go
@@ -2,10 +2,7 @@ package controllers
 
 import (
 	"fmt"
-	"html/template"
 	"net/http"
-
-	"github.com/gorilla/csrf"
 
 	"github.com/Jasstkn/lenslocked/models"
 )
@@ -20,21 +17,17 @@ type Users struct {
 
 func (u Users) New(w http.ResponseWriter, r *http.Request) {
 	var data struct {
-		Email     string
-		CSRFField template.HTML
+		Email string
 	}
 	data.Email = r.FormValue("email")
-	data.CSRFField = csrf.TemplateField(r)
 	u.Templates.New.Execute(w, data)
 }
 
 func (u Users) SignIn(w http.ResponseWriter, r *http.Request) {
 	var data struct {
-		Email     string
-		CSRFField template.HTML
+		Email string
 	}
 	data.Email = r.FormValue("email")
-	data.CSRFField = csrf.TemplateField(r)
 	u.Templates.SignIn.Execute(w, data)
 }
 
