e25bccecf67e1bc4383ab50dd1a610bda863b5da
Mariia Kotliarevskaia <jasstkn.051@gmail.com>
2023-07-23 17:54:47 +0200

    update existing session
    Signed-off-by: Mariia Kotliarevskaia <jasstkn.051@gmail.com>

--- models/session.go
+++ models/session.go
@@ -47,12 +47,27 @@ func (ss *SessionService) Create(userID int) (*Session, error) {
 		TokenHash: ss.hash(token),
 	}
 
+	// 1. Query for a user's session
+	// 2. if found, update the user's session
+	// 3. if not found, create a new session for the user
+
+	// 1. try to update the user's session
+	// 2. if error, create a new session
 	row := ss.DB.QueryRow(`
-		INSERT INTO sessions (user_id, token_hash)
-		VALUES ($1, $2)
-		RETURNING id;`, session.UserID, session.TokenHash)
+		UPDATE sessions
+		SET token_hash = $2
+		WHERE user_id = $1 RETURNING id;`, session.UserID, session.TokenHash)
 
 	err = row.Scan(&session.ID)
+
+	if err == sql.ErrNoRows {
+		row := ss.DB.QueryRow(`
+			INSERT INTO sessions (user_id, token_hash)
+			VALUES ($1, $2)
+			RETURNING id;`, session.UserID, session.TokenHash)
+		err = row.Scan(&session.ID)
+	}
+
 	if err != nil {
 		return nil, fmt.Errorf("create: %w", err)
 	}
