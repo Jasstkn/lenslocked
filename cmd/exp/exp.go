7646db84974f250b805abd685010a379d9e51ede
Mariia Kotliarevskaia <jasstkn.051@gmail.com>
2023-08-10 21:05:19 +0200

    typed context values
    Signed-off-by: Mariia Kotliarevskaia <jasstkn.051@gmail.com>

--- cmd/exp/exp.go
+++ cmd/exp/exp.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"strings"
 )
 
 type ctxKey string
@@ -13,7 +14,16 @@ const (
 
 func main() {
 	ctx := context.Background()
+
 	ctx = context.WithValue(ctx, favoriteColorKey, "blue")
+
 	value := ctx.Value(favoriteColorKey)
-	fmt.Println(value)
+	strValue, ok := value.(string)
+	if !ok {
+		fmt.Println("failed to do type assertion")
+		return
+	}
+
+	fmt.Println(strValue)
+	fmt.Println(strings.HasPrefix(strValue, "b"))
 }
