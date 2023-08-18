bc60f4019dbdfcf8ba084d57c9cd99e5419bca9c
Maria Kotliarevskaia <mariia.kotliarevskaia@gmail.com>
2023-08-18 11:57:43 +0200

    sending emails with smtp (#45)

--- cmd/exp/exp.go
+++ cmd/exp/exp.go
@@ -1,14 +1,21 @@
 package main
 
 import (
-	"os"
+	"fmt"
 
 	"github.com/go-mail/mail/v2"
 )
 
+const (
+	host     = "sandbox.smtp.mailtrap.io"
+	port     = 587
+	username = ""
+	password = ""
+)
+
 func main() {
 	from := "test@lenslocked.com"
-	to := "jasstkn.051@gmail.com"
+	to := "test@gmail.com"
 	subject := "This is a test email"
 	plaintext := "This is the body of the email"
 	html := `<h1>Hello there buddy!</h1><p>This is the email</p><p>Hope you enjoy it</p>`
@@ -20,6 +27,10 @@ func main() {
 	msg.SetBody("text/plain", plaintext)
 	msg.AddAlternative("text/html", html)
 
-	msg.WriteTo(os.Stdout)
-
+	dialer := mail.NewDialer(host, port, username, password)
+	err := dialer.DialAndSend(msg)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Message sent")
 }
