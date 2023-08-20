3140bf9fac5633defb644077a1d0b75b59256eca
Maria Kotliarevskaia <mariia.kotliarevskaia@gmail.com>
2023-08-20 19:29:05 +0200

    use env vars for smtp connections (#49)

--- cmd/exp/exp.go
+++ cmd/exp/exp.go
@@ -2,18 +2,27 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strconv"
 
 	"github.com/Jasstkn/lenslocked/models"
-)
-
-const (
-	host     = "sandbox.smtp.mailtrap.io"
-	port     = 587
-	username = ""
-	password = ""
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		panic(err)
+	}
+
+	host := os.Getenv("SMTP_HOST")
+	port, err := strconv.Atoi(os.Getenv("SMTP_PORT"))
+	if err != nil {
+		panic(err)
+	}
+	username := os.Getenv("SMTP_USERNAME")
+	password := os.Getenv("SMTP_PASSWORD")
+
 	es := models.NewEmailService(models.SMTPConfig{
 		Host:     host,
 		Port:     port,
@@ -21,7 +30,7 @@ func main() {
 		Password: password,
 	})
 
-	err := es.ForgotPassword("test@gmail.com", "https://lenslocked.com/reset-pw?token=abc123")
+	err = es.ForgotPassword("test@gmail.com", "https://lenslocked.com/reset-pw?token=abc123")
 	if err != nil {
 		panic(err)
 	}
