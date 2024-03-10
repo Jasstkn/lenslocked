ca5035953ce69479e3e9c33af96cab8564c0c788
Mariia Kotliarevskaia <jasstkn.051@gmail.com>
2024-03-10 13:55:06 +0100

    feat: dynamic alerts

--- views/template.go
+++ views/template.go
@@ -29,6 +29,13 @@ func ParseFS(fs fs.FS, patterns ...string) (Template, error) {
 			"currentUser": func() (template.HTML, error) {
 				return "", fmt.Errorf("currentUser not implemeneted") //nolint:goerr113
 			},
+			"alerts": func() []string {
+				return []string{
+					"Don't do that!",
+					"The email address you provided is already associated with an account.",
+					"Something went wrong.",
+				}
+			},
 		},
 	)
 
