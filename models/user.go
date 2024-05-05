54589ea1f2fc395f1a9da8a10dcf66e568df77e7
Mariia Kotliarevskaia <mariia.kotliarevskaia@gmail.com>
2024-05-05 19:47:22 +0200

    feat: detect existing email

--- models/user.go
+++ models/user.go
@@ -2,10 +2,17 @@ package models
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
 	"strings"
 
+	"github.com/jackc/pgconn"
+	"github.com/jackc/pgerrcode"
 	"golang.org/x/crypto/bcrypt"
+)
+
+var (
+	ErrEmailTaken = errors.New("models: email already taken")
 )
 
 type User struct {
@@ -36,6 +43,12 @@ func (us *UserService) Create(email, password string) (*User, error) {
 
 	err = row.Scan(&user.ID)
 	if err != nil {
+		var pgError *pgconn.PgError
+		if errors.As(err, &pgError) {
+			if pgError.Code == pgerrcode.UniqueViolation {
+				return nil, ErrEmailTaken
+			}
+		}
 		return nil, fmt.Errorf("create user: %w", err)
 	}
 
